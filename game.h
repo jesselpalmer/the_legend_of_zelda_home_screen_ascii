f43bac2e80ac8c6f65dc464e1efc9e7f5790bb18
Jesse Palmer <jesselpalmer@gmail.com>
2020-02-06 22:27:18 -0800

    refactor: created header file

---
+++ game.h
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Game {
+    public:
+        void drawScreen();
+};
+
+void Game::drawScreen() {
+    std::cout << "__                                                      __"<< std::endl;
+    std::cout << "| |                                                    | |"<< std::endl;
+    std::cout << "| |       *************************************        | |"<< std::endl;
+    std::cout << "| |_      *         **--------------          *        | |"<< std::endl;
+    std::cout << "|   |     *      THE LEGEND OF    /           *       _| |"<< std::endl;
+    std::cout << "|   |     *    Z    E    L    D    A          *      |   |"<< std::endl;
+    std::cout << "|   |     *              \\    /               *      |   |"<< std::endl;
+    std::cout << "|   |     *  <=========================:::::  *      |   |"<< std::endl;
+    std::cout << "|   |___  *                \\/          `---´  *   ___|   |"<< std::endl;
+    std::cout << "|      |  *************************************   |      |"<< std::endl;
+    std::cout << "|      |                 © 1986 NINTENDO          |      |"<< std::endl;
+    std::cout << "|      |                                          |      |"<< std::endl;
+    std::cout << "|      |__          PUSH START BUTTON           __|      |"<< std::endl;
+    std::cout << "|         |_                                  _|         |"<< std::endl;
+    std::cout << "|           |___          _____           __|            |"<< std::endl;
+    std::cout << "|               |~~~~~~~~|     |___   ____|              |"<< std::endl;
+    std::cout << "|               |~~~~~~~~|         |_|                   |"<< std::endl;
+    std::cout << "|               |~~~~~~~~|                               |"<< std::endl;
+    std::cout << "|               |~~~~~~~~|                               |"<< std::endl;
+    std::cout << "|               |~~~~~~~~|                               |"<< std::endl;
+}
